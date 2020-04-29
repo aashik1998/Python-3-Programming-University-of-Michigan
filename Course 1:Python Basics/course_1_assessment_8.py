a14079f16f7de8a92ed12f89970bf425f168da05
Aashik M S <aashikms25@gmail.com>
2020-04-29 22:57:09 +0530

    first commit

---
+++ Course 1:Python Basics/course_1_assessment_8.py
@@ -0,0 +1,20 @@
+'''
+seqmut-1-5: Could aliasing cause potential confusion in this problem?
+   yes
+
+seqmut-1-6: Could aliasing cause potential confusion in this problem?
+   no
+
+seqmut-1-1: Which of these is a correct reference diagram following the execution of the following code?
+
+  A.I
+
+seqmut-1-7: Which of these is a correct reference diagram following the execution of the following code?
+  
+  D IV
+
+seqmut-1-8: Which of these is a correct reference diagram following the execution of the following code?
+
+  A.I
+  '''
+ 
